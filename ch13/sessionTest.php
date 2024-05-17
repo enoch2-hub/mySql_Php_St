3dc51abdce77feccc68ab500e0d54fe0b2449d52
enoch2-hub <enoch.perera.coding2@gmail.com>
2024-05-17 13:46:48 +0530

    prvnt session fixation

--- ch13/sessionTest.php
+++ ch13/sessionTest.php
@@ -4,7 +4,39 @@
 
 // Example 13-9. A session susceptible to session fixation
 
+    // session_start();
+
+    // if(!isset($_SESSION['count'])) {
+    //     $_SESSION['count'] = 0;
+    // } else {
+    //     ++$_SESSION['count'];
+    // }
+
+    // echo $_SESSION['count'];
+
+
+
+//------------------
+
+// Example 13-10. Session regeneration
+
+    session_start();
     
+    if(!isset($_SESSION['initiated'])) {
+        session_regenerate_id();
+        $_SESSION['initiated'] = 1;
+    }
+
+    if(!isset($_SESSION['count'])) {
+        $_SESSION['count'] = 0;
+    } else {
+        ++$_SESSION['count'];
+    }
+
+    echo $_SESSION['count'];
+
+
+
 
 
 ?>
